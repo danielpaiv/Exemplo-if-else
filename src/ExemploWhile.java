ba45c7717e5d49dca337a1671640828aba6f11f0
Daniel Araujo <dpaiva266@hotmail.com>
2024-03-09 15:53:35 -0300

    *Comandos while

---
+++ src/ExemploWhile.java
@@ -0,0 +1,9 @@
+public class ExemploWhile {
+    public static void main(String args[]){
+        int count =0;
+        while (count < 10){
+            System.out.println("repetição nr:" + count);
+            count++;//seem esse comando o a contagem fica infinita lupe infinito
+        }
+    }
+}
